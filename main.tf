dc530705aef4edbb36949aa4466237023be850bc
rainrajon <147498777+rainrajon@users.noreply.github.com>
2023-10-31 13:25:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
 resource "azurerm_resource_group" "rg" {
  name     = "rg-200600-sc-NonProd"
  location = "eastus"
